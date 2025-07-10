a5a1d058a25714395b3d0146f797918aa2161332
vinojenavs <vinojenavs02@gmail.com@byupathway.edu>
2025-07-10 21:44:12 +0100

    Week 01 AAssignment

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+function displayCurrentYear() { document.getElementById("currentyear").innerHTML = new Date().getFullYear(); }
+displayCurrentYear();
+
+document.getElementById("lastModified").innerHTML = "Last Updated" + document.lastModified;

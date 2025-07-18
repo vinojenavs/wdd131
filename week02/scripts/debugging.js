2ea752a7f2bb932018f06d75f0617ba123c458ad
vinojenavs <vinojenavs02@gmail.com@byupathway.edu>
2025-07-18 15:32:05 +0100

    Week 02 debugging activities

---
+++ week02/scripts/debugging.js
@@ -0,0 +1,11 @@
+const radiusOutput = document.getElementById('radius');
+const areaOutput = document.querySelector('area');
+
+let area = 0;
+const PI = 3.14159;
+
+const radius = 10;
+area = PI * radius * radius;
+radiusOutput.textContent = radius;
+
+area = PI * radius * radius;

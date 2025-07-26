d5088f4b9f895dcb6759bdd8509e3c07630316dc
zinojenavs <eajenavi@byupathway.edu>
2025-07-26 15:23:54 +0100

    week 03 ass correction

--- scripts/place.js
+++ scripts/place.js
@@ -10,9 +10,7 @@ if (temp <= 10, speed > 4.8) {
 const chill = calculateWindChill(temp, speed);
 windChill.textContent = chill.toFixed(1);
 
-function calculateWindChill(temp, speed) {
-    return 35.74 + (0.6215 * temp) - (35.75 * (speed ** 0.16)) + (0.4275 * temp * (speed ** 0.16));
-}
+function calculateWindChill(temp, speed) { return 35.74 + (0.6215 * temp) - (35.75 * (speed ** 0.16)) + (0.4275 * temp * (speed ** 0.16));}
 
 function displayCurrentYear() { document.getElementById("currentyear").innerHTML = new Date().getFullYear(); }
 displayCurrentYear();
